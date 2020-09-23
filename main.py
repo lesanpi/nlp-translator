c12dd985b7105519f4dcea3ae3ab4b54705152dd
Luis Sanchez <lespinerua@gmail.com>
2020-09-23 08:34:52 -0400

    Mecanismo de Atencion

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 from utils import *
 from dataset import Dataset
 from RNN import Encoder, Decoder
+from AttnRNN import AttnDecoder, AttnEncoder
 from tqdm import tqdm
 import numpy as np
 
@@ -47,6 +48,9 @@ hidden = encoder(torch.randint(0, input_lang.n_words, (64, 10)))
 decoder = Decoder(input_size=output_lang.n_words)
 output, decoder_hidden = decoder(torch.randint(
     0, output_lang.n_words, (64, 1)), hidden)
+
+attnEncoder = AttnEncoder(input_size=input_lang.n_words)
+attnDecoder = AttnDecoder(input_size=output_lang.n_words)
 
 
 def fit(encoder, decoder, dataloader, epochs=10):
@@ -106,3 +110,99 @@ def fit(encoder, decoder, dataloader, epochs=10):
                 val_loss.append(loss.item())
                 bar.set_description(
                     f"Epoch {epoch}/{epochs} val_loss {np.mean(val_loss):.5f}")
+
+
+def fitATTN(encoder, decoder, dataloader, epochs=10):
+    encoder.to(device)
+    decoder.to(device)
+    encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr=1e-3)
+    decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=1e-3)
+    criterion = torch.nn.CrossEntropyLoss()
+    for epoch in range(1, epochs+1):
+        encoder.train()
+        decoder.train()
+        train_loss = []
+        bar = tqdm(dataloader['train'])
+        for batch in bar:
+            input_sentences, output_sentences = batch
+            bs = input_sentences.shape[0]
+            loss = 0
+            encoder_optimizer.zero_grad()
+            decoder_optimizer.zero_grad()
+            # obtenemos el último estado oculto del encoder
+            encoder_outputs, hidden = encoder(input_sentences)
+            # calculamos las salidas del decoder de manera recurrente
+            decoder_input = torch.tensor(
+                [[output_lang.word2index['SOS']] for b in range(bs)], device=device)
+            for i in range(output_sentences.shape[1]):
+                output, hidden, attn_weights = decoder(
+                    decoder_input, hidden, encoder_outputs)
+                loss += criterion(output, output_sentences[:, i].view(bs))
+                # el siguiente input será la palabra predicha
+                decoder_input = torch.argmax(output, axis=1).view(bs, 1)
+            # optimización
+            loss.backward()
+            encoder_optimizer.step()
+            decoder_optimizer.step()
+            train_loss.append(loss.item())
+            bar.set_description(
+                f"Epoch {epoch}/{epochs} loss {np.mean(train_loss):.5f}")
+
+        val_loss = []
+        encoder.eval()
+        decoder.eval()
+        with torch.no_grad():
+            bar = tqdm(dataloader['test'])
+            for batch in bar:
+                input_sentences, output_sentences = batch
+                bs = input_sentences.shape[0]
+                loss = 0
+                # obtenemos el último estado oculto del encoder
+                encoder_outputs, hidden = encoder(input_sentences)
+                # calculamos las salidas del decoder de manera recurrente
+                decoder_input = torch.tensor(
+                    [[output_lang.word2index['SOS']] for b in range(bs)], device=device)
+                for i in range(output_sentences.shape[1]):
+                    output, hidden, attn_weights = decoder(
+                        decoder_input, hidden, encoder_outputs)
+                    loss += criterion(output, output_sentences[:, i].view(bs))
+                    # el siguiente input será la palabra predicha
+                    decoder_input = torch.argmax(output, axis=1).view(bs, 1)
+                val_loss.append(loss.item())
+                bar.set_description(
+                    f"Epoch {epoch}/{epochs} val_loss {np.mean(val_loss):.5f}")
+
+
+print("Muestra")
+print(dataset['train'][129])
+#fit(encoder, decoder, dataloader, epochs=30)
+
+MAX_LENGTH = 10
+
+
+def predict(input_sentence):
+    # obtenemos el último estado oculto del encoder
+    hidden = encoder(input_sentence.unsqueeze(0))
+    # calculamos las salidas del decoder de manera recurrente
+    decoder_input = torch.tensor(
+        [[output_lang.word2index['SOS']]], device=device)
+    # iteramos hasta que el decoder nos de el token <eos>
+    outputs = []
+    while True:
+        output, hidden = decoder(decoder_input, hidden)
+        decoder_input = torch.argmax(output, axis=1).view(1, 1)
+        outputs.append(decoder_input.cpu().item())
+        if decoder_input.item() == output_lang.word2index['EOS']:
+            break
+    return output_lang.sentenceFromIndex(outputs)
+
+
+def translate(sentence):
+    sn = input_lang.indexesFromSentence(sentence)
+    sn = torch.Tensor(sn)
+    sn = sn.type(torch.long)
+    print(sn)
+    return predict(sn)
+
+
+translate("my name is luis")
